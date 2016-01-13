a91a972001cdadb2f162446b8ce861f8d1cb4db8
Tiago Ferreira Bonetti <tiago.bonetti@gmail.com>
2016-01-12 23:44:11 -0200

    Base Input and Controller Input

---
+++ Assets/Scripts/Input/BaseInput.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseInput {
+    public virtual float horizontal { get; protected set; }
+    public virtual float vertical { get; protected set; }
+    public virtual bool explode { get; protected set; }
+
+    public BaseInput() {
+        horizontal = 0.0f;
+        vertical = 0.0f;
+        explode = false;
+    }
+
+    // must be called each Update before consuming any input.
+    public virtual void Update() {
+    }
+
+    // must be called each FixedUpdate after consuming all input;
+    public virtual void FixedUpdate() {
+    }
+}
---
+++ Assets/Scripts/Input/ControllerInput.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Controller : int {
+    One = 1,
+    //Two = 2,
+    //Three = 3,
+    //Four = 4,
+}
+
+public class ControllerInput : BaseInput {
+    // private input data
+    private float _horizontal = 0f;
+    private float _vertical = 0f;
+    private bool _explode = false;
+
+    // public input proprieties
+    public override float horizontal {
+        get {
+            CheckUpdated();
+            return _horizontal;
+        }
+        protected set {
+            _horizontal = value;
+        }
+    }
+    public override float vertical {
+        get {
+            CheckUpdated();
+            return _vertical;
+        }
+        protected set { _vertical = value; }
+    }
+    public override bool explode {
+        get {
+            CheckUpdated();
+            return _explode;
+        }
+        protected set { _explode = value; }
+    }
+
+    private bool updated = false;
+    private void CheckUpdated() {
+        if (!updated) {
+            Debug.Log("Input read before updated");
+        }
+    }
+
+    // config controller to be read
+    public Controller index = Controller.One;
+
+    public override void Update() {
+        horizontal = Input.GetAxis("HorizontalC" + (int)index);
+        vertical = Input.GetAxis("VerticalC" + (int)index);
+        explode |= Input.GetButtonDown("ExplodeC" + (int)index);
+        updated = true;
+    }
+
+    public override void FixedUpdate() {
+        updated = false;
+    }
+}
