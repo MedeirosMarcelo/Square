bbdf5cb1aad9a1971c6cf0465ffa6b1cff82338c
MedeirosMarcelo <marcelomedeiros-@hotmail.com>
2016-02-29 02:20:16 -0300

    Fire

---
+++ Assets/Scripts/Map/LightFlicker.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//[RequireComponent(typeof(Light))]
+public class LightFlicker : MonoBehaviour {
+
+    public float minIntensity = 0.25f;
+    public float maxIntensity = 0.1f;
+    public Light[] light;
+
+    float random;
+    float randomTime;
+    float intensity;
+    bool flicker;
+    Timer timer = new Timer();
+
+    void Start() {
+        flicker = true;
+    }
+
+    void Update() {
+        if (flicker) {
+            random = Random.Range(2.8f, 3.81f);
+            for (int i = 0; i < light.Length; i++) {
+                light[i].intensity = random;
+            }
+            flicker = false;
+            randomTime = Random.Range(0.1f, 0.5f);
+        }
+        else {
+            DelayFlick(randomTime);
+        }
+    }
+
+    void DelayFlick(float delay) {
+        if (timer.Run(delay)) {
+            flicker = true;
+        }
+    }
+}
