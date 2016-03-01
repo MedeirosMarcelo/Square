eb57775ea40b5da0df6545b15faf5fff505a059e
MedeirosMarcelo <marcelomedeiros-@hotmail.com>
2016-02-29 22:32:56 -0300

    LightFlicker

--- Assets/Scripts/Map/LightFlicker.cs
+++ Assets/Scripts/Map/LightFlicker.cs
@@ -1,40 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-//[RequireComponent(typeof(Light))]
 public class LightFlicker : MonoBehaviour {
 
-    public float minIntensity = 0.25f;
-    public float maxIntensity = 0.1f;
-    public Light[] light;
-
-    float random;
-    float randomTime;
-    float intensity;
-    bool flicker;
-    Timer timer = new Timer();
+    public Light light;
+    public float amount = -0.15f;
+    public float speed = 1.88f;
+    private Color originalColor;
+    private float timePassed;
+    private float changeValue;
 
     void Start() {
-        flicker = true;
+
+        if (light != null) {
+            originalColor = light.color;
+        }
+        else {
+            enabled = true;
+            return;
+        }
+
+        changeValue = 0;
+        timePassed = 0;
     }
 
     void Update() {
-        if (flicker) {
-            random = Random.Range(2.8f, 3.81f);
-            for (int i = 0; i < light.Length; i++) {
-                light[i].intensity = random;
-            }
-            flicker = false;
-            randomTime = Random.Range(0.1f, 0.5f);
-        }
-        else {
-            DelayFlick(randomTime);
-        }
+        timePassed = Time.time;
+        timePassed = timePassed - Mathf.Floor(timePassed);
+
+        light.color = originalColor * CalculateChange();
     }
 
-    void DelayFlick(float delay) {
-        if (timer.Run(delay)) {
-            flicker = true;
-        }
+    float CalculateChange() {
+        changeValue = -Mathf.Sin(timePassed * 12 * Mathf.PI) * amount + speed;
+        return changeValue;
     }
 }
